7dc4efa533f2cecf81a1b7264ddd42edcb376889
Ericwiz <wisdomagu44@gmail.com>
2022-09-28 03:48:58 +0100

    more code

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+const modal = document.getElementById('modal');
+const modalCloseBtn = document.getElementById('modal-close-btn');
+const form = document.getElementById('consent-form');
+
 function setTime() {
-    document.getElementById('modal').style.display = 'block';
+    modal.style.display = 'inline';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
 }
 
 setTimeout(setTime, 1500)
+modalCloseBtn.addEventListener('click', closeModal)
+form.addEventListener('submit', e => {
+    e.preventDefault()
+})
